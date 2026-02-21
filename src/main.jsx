18f51b276930f89ef96f1c342f149ad3a77c17a2
Yevheniia Cherepashchuk <chtozalogin.jh@gmail.com>
2026-02-21 17:49:10 +0100

    init

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import ReactDOM from "react-dom";
+
+import "./index.css";
+import App from "./App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import App from "./App";
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// );
